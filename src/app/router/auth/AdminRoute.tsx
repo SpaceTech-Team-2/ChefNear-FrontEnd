9f156f5e0f32782644a2a0535234211b183adacc
Ahmed-Mohammed-Marwan <ahmed.flash5555@gmail.com>
2026-08-22 14:22:49 +0300

    feat: update AdminRoute to fetch user profile and handle loading state

--- src/app/router/auth/AdminRoute.tsx
+++ src/app/router/auth/AdminRoute.tsx
@@ -1,12 +1,28 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getMyProfile } from "../../../services/api";
 
+export const  AdminRoute  =  () => {
+  const [profile, setProfile] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
 
-export const AdminRoute = () => {
-  const token = localStorage.getItem("admin");
+  useEffect(() => {
+    const fetchProfile = async () => {
+      const response = await getMyProfile();
+      console.log("test ", response?.data?.role);
+      setProfile(response);
+      setLoading(false);
+    };
+    fetchProfile();
+  }, []);
 
-  if (token) {
-      return <Outlet />;
-    }
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+// Admin; 
+  if (profile?.data?.role == "Chef") {
+    return <Outlet />;
+  }
     
-    return <Navigate to="/" replace />;
+  return <Navigate to="/" replace />;
 };
